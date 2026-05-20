5caf961d27d8f28daaf7e5ac78a133c5402b01fd
Daehee Han <gkseogml4959@gmail.com>
2026-05-20 22:38:45 +0900

    refactor: TypingInput 컴포넌트 분리

---
+++ src/pages/Play/components/TypingInput.tsx
@@ -0,0 +1,26 @@
+import { forwardRef } from "react";
+
+type TypingInputProps = {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+};
+
+const TypingInput = forwardRef<HTMLInputElement, TypingInputProps>(
+  ({ value, onChange, onKeyDown }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type="text"
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        className="w-full px-4 py-3 border-3 border-[#7CB9E8] rounded-xl text-2xl leading-relaxed mb-6 focus:outline-none"
+      />
+    );
+  },
+);
+
+TypingInput.displayName = "TypingInput";
+
+export default TypingInput;
