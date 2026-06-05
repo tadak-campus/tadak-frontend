b24209c8977a96959f05afd0859cd108dfd28101
Daehee Han <gkseogml4959@gmail.com>
2026-06-05 15:28:16 +0900

    docs: UserMe의 공유 타입 의존 정리 TODO 주석 추가

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,5 @@
+// TODO: EquippedItems 등 공유 도메인 타입은 추후 src/types/shop.ts로 이전해
+//       타입 레이어가 페이지 모듈(@pages/Shop)에 의존하지 않도록 정리한다.
 import type { EquippedItems } from "@pages/Shop/shopData";
 
 // GET /api/users/me 응답. equipped_items의 각 슬롯은 ShopItem | null.
